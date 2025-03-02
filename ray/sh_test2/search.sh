eb3e985b6b223be69712bc46f2522278c3f5eb5a
彭子豪 <23373390@buaa.edu.cn>
2025-03-02 22:03:30 +0800

    commit all files

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
+source=$1
+pattern=$2
+output=$3
+grep -n "$pattern" $source | awk -F ':' '{print $1}' > $output
