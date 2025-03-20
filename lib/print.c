bfe0c3b8ddec8ce379c88bbbe50b0cb7ff4bf8aa
彭子豪 <23373390@buaa.edu.cn>
2025-03-20 18:31:44 +0800

    copy solutions

--- lib/print.c
+++ lib/print.c
@@ -38,6 +38,74 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 		/* check for long */
 		/* Exercise 1.4: Your code here. (7/8) */
 
+		/* scan for the next '%' */
+		/* Exercise 1.4: Your code here. (1/8) */
+		int length = 0;
+		s = fmt;
+		for (; *fmt != '\0'; fmt++)
+		{
+			if (*fmt != '%')
+			{
+				length++;
+			}
+			else
+			{
+				out(data, s, length);
+				length = 0;
+				fmt++;
+				break;
+			}
+		}
+
+		/* flush the string found so far */
+		/* Exercise 1.4: Your code here. (2/8) */
+		out(data, s, length);
+
+		/* check "are we hitting the end?" */
+		/* Exercise 1.4: Your code here. (3/8) */
+
+		if (!*fmt)
+		{
+			break;
+		}
+
+		/* we found a '%' */
+		/* Exercise 1.4: Your code here. (4/8) */
+		ladjust = 0;
+		padc = ' ';
+
+		/* check format flag */
+		/* Exercise 1.4: Your code here. (5/8) */
+		if (*fmt == '-')
+		{
+			ladjust = 1;
+			padc = ' ';
+			fmt++;
+		}
+		else if (*fmt == '0')
+		{
+			ladjust = 0;
+			padc = '0';
+			fmt++;
+		}
+
+		/* get width */
+		/* Exercise 1.4: Your code here. (6/8) */
+		width = 0;
+		while ((*fmt >= '0') && (*fmt <= '9'))
+		{
+			width = width * 10 + (*fmt) - '0';
+			fmt++;
+		}
+
+		/* check for long */
+		/* Exercise 1.4: Your code here. (7/8) */
+		long_flag = 0;
+		while (*fmt == 'l')
+		{
+			long_flag = 1;
+			fmt++;
+		}
 		neg_flag = 0;
 		switch (*fmt) {
 		case 'b':
@@ -64,6 +132,9 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 			 */
 			/* Exercise 1.4: Your code here. (8/8) */
 
+			neg_flag = num < 0;
+			num = neg_flag ? -num : num;
+			print_num(out, data, num, 10, neg_flag, width, ladjust, padc, 0);
 			break;
 
 		case 'o':
