89522bdf35a28fcd08e19f8750d677f7ccdd4fe8
彭子豪 <23373390@buaa.edu.cn>
2025-04-20 00:35:53 +0800

    first finish lab3

--- lib/elfloader.c
+++ lib/elfloader.c
@@ -1,11 +1,14 @@
 #include <elf.h>
 #include <pmap.h>
 
-const Elf32_Ehdr *elf_from(const void *binary, size_t size) {
+/* 验证给定的二进制文件是否为可执行文件并返回ELF头*/
+const Elf32_Ehdr *elf_from(const void *binary, size_t size)
+{
 	const Elf32_Ehdr *ehdr = (const Elf32_Ehdr *)binary;
 	if (size >= sizeof(Elf32_Ehdr) && ehdr->e_ident[EI_MAG0] == ELFMAG0 &&
-	    ehdr->e_ident[EI_MAG1] == ELFMAG1 && ehdr->e_ident[EI_MAG2] == ELFMAG2 &&
-	    ehdr->e_ident[EI_MAG3] == ELFMAG3 && ehdr->e_type == 2) {
+		ehdr->e_ident[EI_MAG1] == ELFMAG1 && ehdr->e_ident[EI_MAG2] == ELFMAG2 &&
+		ehdr->e_ident[EI_MAG3] == ELFMAG3 && ehdr->e_type == 2)
+	{
 		return ehdr;
 	}
 	return NULL;
@@ -22,36 +25,47 @@ const Elf32_Ehdr *elf_from(const void *binary, size_t size) {
  *   Return 0 if success. Otherwise return < 0.
  *   If success, the entry point of `binary` will be stored in `start`
  */
-int elf_load_seg(Elf32_Phdr *ph, const void *bin, elf_mapper_t map_page, void *data) {
+int elf_load_seg(Elf32_Phdr *ph, const void *bin, elf_mapper_t map_page, void *data)
+{
 	u_long va = ph->p_vaddr;
 	size_t bin_size = ph->p_filesz;
 	size_t sgsize = ph->p_memsz;
-	u_int perm = PTE_V;
-	if (ph->p_flags & PF_W) {
+	u_int perm = PTE_V; /* 由段的权限位设置页面的权限位 */
+	if (ph->p_flags & PF_W)
+	{
 		perm |= PTE_D;
 	}
 
 	int r;
 	size_t i;
 	u_long offset = va - ROUNDDOWN(va, PAGE_SIZE);
-	if (offset != 0) {
+	/* 处理未对齐的前缀部分*/
+	if (offset != 0)
+	{
 		if ((r = map_page(data, va, offset, perm, bin,
-				  MIN(bin_size, PAGE_SIZE - offset))) != 0) {
+						  MIN(bin_size, PAGE_SIZE - offset))) != 0)
+		{
 			return r;
 		}
 	}
 
 	/* Step 1: load all content of bin into memory. */
-	for (i = offset ? MIN(bin_size, PAGE_SIZE - offset) : 0; i < bin_size; i += PAGE_SIZE) {
+	/* 将data和text段加载进内存*/
+	for (i = offset ? MIN(bin_size, PAGE_SIZE - offset) : 0; i < bin_size; i += PAGE_SIZE)
+	{
 		if ((r = map_page(data, va + i, 0, perm, bin + i, MIN(bin_size - i, PAGE_SIZE))) !=
-		    0) {
+			0)
+		{
 			return r;
 		}
 	}
 
 	/* Step 2: alloc pages to reach `sgsize` when `bin_size` < `sgsize`. */
-	while (i < sgsize) {
-		if ((r = map_page(data, va + i, 0, perm, NULL, MIN(sgsize - i, PAGE_SIZE))) != 0) {
+	/* bss段用0填充 */
+	while (i < sgsize)
+	{
+		if ((r = map_page(data, va + i, 0, perm, NULL, MIN(sgsize - i, PAGE_SIZE))) != 0)
+		{
 			return r;
 		}
 		i += PAGE_SIZE;
