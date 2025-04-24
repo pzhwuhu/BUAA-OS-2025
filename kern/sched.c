49c79e5665642cad94850d8180072ff6d18bfbad
彭子豪 <23373390@buaa.edu.cn>
2025-04-24 12:22:29 +0000

    delete debug info

--- kern/sched.c
+++ kern/sched.c
@@ -63,7 +63,7 @@ void schedule(int yield) {
 		}
 	}
 	if(min_deadline != -1) {
-		printk("edf sched runtimeLeft: %d\n", min_env->env_runtime_left);
+		//printk("edf sched runtimeLeft: %d\n", min_env->env_runtime_left);
 		min_env->env_runtime_left--;
 		env_run(min_env);
 	}
